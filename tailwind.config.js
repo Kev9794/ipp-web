3279e3492815487394748aea102bd64c6d96167d
Kev9794 <k.natanael88@gmail.com>
2024-04-23 16:20:25 +0700

    complete setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -102,7 +104,7 @@ module.exports = {
       },
     },
   },
-  darkMode: "class",
+  // darkMode: "class",
   plugins: [
     require("daisyui"),
     require("tailwind-scrollbar"),
