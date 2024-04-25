3be871c724b7b9248968dd8ec8122110ff89b812
Kev9794 <k.natanael88@gmail.com>
2024-04-25 23:02:06 +0700

    add section landing - hero section

--- app/page.jsx
+++ app/page.jsx
@@ -13,5 +13,36 @@ import {
 } from "@nextui-org/react";
 
 export default function Home() {
-  return <MainNavbar />;
+  const divStyle = {
+    backgroundImage: "url(image/bg1.jpg)", // path to your image
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+    height: "900px", // Adjust height as needed
+  };
+  return (
+    <div>
+      <MainNavbar />
+      <div className="flex w-full">
+        <div style={divStyle}>
+          <div className="flex items-center justify-between px-12 py-10">
+            <div className="w-1/2">
+              <p className="text-5xl font-thin ">
+                Kami berkomitmen untuk selalu meningkatkan produk kami,
+                <br /> dengan menjaga kualitas produk,
+                <br /> dan pengerjaan tepat waktu.
+              </p>
+              <p className="pt-10 font-extralight">
+                Kami menjual Pagar Beton dan Pagar BRC kualitas terbaik dan
+                kokoh.
+              </p>
+            </div>
+            <div>
+              <Image src="image/worker.png" width={650} radius="lg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
