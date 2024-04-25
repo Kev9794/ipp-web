4f128bdb9c463e2bb2824904d21aa13c498eac49
Kev9794 <k.natanael88@gmail.com>
2024-04-25 19:06:38 +0700

    fixed navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,9 +1,13 @@
+"use client";
 import React from "react";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
   Link,
   Button,
   DropdownItem,
@@ -12,96 +16,104 @@ import {
   DropdownMenu,
   Image,
 } from "@nextui-org/react";
-import {
-  ChevronDown,
-  Lock,
-  Activity,
-  Flash,
-  Server,
-  TagUser,
-  Scale,
-} from "./Icons.jsx";
+import { FaAngleDown } from "react-icons/fa6";
 
 export const MainNavbar = () => {
-  const icons = {
-    chevron: <ChevronDown fill="currentColor" size={16} />,
-  };
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
   return (
-    <Navbar>
-      <NavbarBrand>
+    <Navbar className="h-32" onMenuOpenChange={setIsMenuOpen}>
+      <NavbarBrand className="hidden sm:flex">
         <Image src="logo/companylogonavbar.png" height={40} width={100} />
         <p className="font-bold text-inherit">PT Irkon Panelindo Produk</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <Dropdown>
-          <NavbarItem>
-            <DropdownTrigger>
-              <Button
-                disableRipple
-                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
-                endContent={icons.chevron}
-                radius="sm"
-                variant="light"
-              >
-                Features
-              </Button>
-            </DropdownTrigger>
-          </NavbarItem>
-          <DropdownMenu
-            aria-label="ACME features"
-            className="w-[340px]"
-            itemClasses={{
-              base: "gap-4",
-            }}
-          >
-            <DropdownItem
-              key="autoscaling"
-              description="ACME scales apps to meet user demand, automagically, based on load."
-              startContent={icons.scale}
-            >
-              Autoscaling
-            </DropdownItem>
-            <DropdownItem
-              key="usage_metrics"
-              description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
-              startContent={icons.activity}
-            >
-              Usage Metrics
-            </DropdownItem>
-            <DropdownItem
-              key="production_ready"
-              description="ACME runs on ACME, join us and others serving requests at web scale."
-              startContent={icons.flash}
-            >
-              Production Ready
-            </DropdownItem>
-            <DropdownItem
-              key="99_uptime"
-              description="Applications stay on the grid with high availability and high uptime guarantees."
-              startContent={icons.server}
-            >
-              +99% Uptime
-            </DropdownItem>
-            <DropdownItem
-              key="supreme_support"
-              description="Overcome any challenge with a supporting team ready to respond."
-              startContent={icons.user}
-            >
-              +Supreme Support
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
+      <NavbarContent className="hidden sm:flex gap-6" justify="center">
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
-            Customers
+            Homepage
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-            Integrations
+            Service
+          </Link>
+        </NavbarItem>
+        <Dropdown className="bg-zinc-700">
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                endContent={<FaAngleDown />}
+                radius="sm"
+                variant="light"
+              >
+                Projects
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu aria-label="ACME features" className="w-[340px]">
+            <DropdownItem key="all_project">All Projects</DropdownItem>
+            <DropdownItem key="usage_metrics">Pagar</DropdownItem>
+            <DropdownItem key="production_ready">Pagar</DropdownItem>
+            <DropdownItem key="99_uptime">Pagar</DropdownItem>
+            <DropdownItem key="supreme_support">Pagar</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About Us
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact Us
           </Link>
         </NavbarItem>
       </NavbarContent>
+
+      <NavbarContent className="flex sm:hidden">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <Image src="logo/companylogonavbar.png" height={40} width={100} />
+          <p className="font-bold text-inherit">PT Irkon Panelindo Produk</p>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2
+                  ? "primary"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 };
