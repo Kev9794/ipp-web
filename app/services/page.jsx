158e7efa1c74842cf98d67d3c224fe92ade5863e
irbaadika123 <irbaadika123@gmail.com>
2024-05-29 14:30:53 +0700

    Update all service page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -3,6 +3,37 @@ import Link from "next/link";
 import { Image, Button } from "@nextui-org/react";
 
 export default function OurService() {
+  const catalogueData = [
+    {
+      title: "Pagar Panel Beton",
+      image: "pagar-beton-1.jpg",
+      redirectUrl: "/pagar-beton",
+    },
+    { title: "U-Ditch", image: "uditch-1.jpg", redirectUrl: "/u-ditch" },
+    {
+      title: "Box Culvert",
+      image: "box-culvert-1.jpg",
+      redirectUrl: "/box-culvert",
+    },
+    { title: "Kanstin", image: "kanstin-5.jpeg", redirectUrl: "/kanstin" },
+    {
+      title: "Buis Beton",
+      image: "buis-beton-1.jpg",
+      redirectUrl: "/buis-beton",
+    },
+    { title: "Pipa RCP", image: "pipa-rcp-1.jpg", redirectUrl: "/pipa-rcp" },
+    {
+      title: "Paving Block",
+      image: "paving-block-1.jpg",
+      redirectUrl: "/paving-block",
+    },
+    {
+      title: "Pagar Kawat Berduri",
+      image: "pagar-kawat-1.jpg",
+      redirectUrl: "/pagar-kawat",
+    },
+    { title: "Pagar BRC", image: "pagar-brc-1.jpg", redirectUrl: "/pagar-brc" },
+  ];
   return (
     <>
       <div className="flex flex-col p-14 bg-zinc-800">
@@ -11,67 +42,33 @@ export default function OurService() {
             Our <span className="text-red-600">Service</span>
           </p>
         </div>
-        <div className="flex flex-row gap-x-4">
-          <div className="card w-1/5 bg-base-100 shadow-xl">
-            <figure>
-              <img src="/image/pagarBetonService.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Pagar Panel Beton</h2>
-              <p>Rp. 110.000</p>
-              <div className="card-actions justify-end">
-                <Button color="primary">Learn More</Button>
+        <div className="flex flex-row flex-wrap justify-center gap-4">
+          {catalogueData.map((item) => (
+            <div key={item} className="card w-1/5 bg-base-100 shadow-xl ">
+              <div className="relative bg-red10 w-full h-[190px] rounded-t-[16px]">
+                <div
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 aspect-w-[3] aspect-h-[3]"
+                  style={{ width: "150px", height: "150px" }}
+                >
+                  <img
+                    src={`/image/product/${item.image}`}
+                    alt={item.title}
+                    className="object-cover w-full h-full rounded-[8px]"
+                  />
+                </div>
+              </div>
+              <div className="card flex flex-col flex-1 p-4 flex-grow">
+                <h2 className="card-title whitespace-nowrap mb-2">
+                  {item.title}
+                </h2>
+                <div className="card-actions justify-end">
+                  <Link href={`/services/${item.redirectUrl}`}>
+                    <Button color="primary">Learn More</Button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="card w-1/5 bg-base-100 shadow-xl">
-            <figure>
-              <img src="/image/pagarBetonService.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Pagar Panel Beton</h2>
-              <p>Rp. 110.000</p>
-              <div className="card-actions justify-end">
-                <Button color="primary">Learn More</Button>
-              </div>
-            </div>
-          </div>
-          <div className="card w-1/5 bg-base-100 shadow-xl">
-            <figure>
-              <img src="/image/pagarBetonService.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Pagar Panel Beton</h2>
-              <p>Rp. 110.000</p>
-              <div className="card-actions justify-end">
-                <Button color="primary">Learn More</Button>
-              </div>
-            </div>
-          </div>
-          <div className="card w-1/5 bg-base-100 shadow-xl">
-            <figure>
-              <img src="/image/pagarBetonService.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Pagar Panel Beton</h2>
-              <p>Rp. 110.000</p>
-              <div className="card-actions justify-end">
-                <Button color="primary">Learn More</Button>
-              </div>
-            </div>
-          </div>
-          <div className="card w-1/5 bg-base-100 shadow-xl">
-            <figure>
-              <img src="/image/pagarBetonService.png" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Pagar Panel Beton</h2>
-              <p>Rp. 110.000</p>
-              <div className="card-actions justify-end">
-                <Button color="primary">Learn More</Button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
