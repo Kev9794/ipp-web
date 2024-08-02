e2151d64b33bc75de1c7805dd4dc8ace406aa81f
Kev9794 <k.natanael88@gmail.com>
2024-08-02 23:07:53 +0700

    add pdf viewer

---
+++ app/components/PdfViewer.js
@@ -0,0 +1,25 @@
+"use client";
+
+import React from 'react';
+import { Worker, Viewer, SpecialZoomLevel } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+const PdfViewer = ({ fileUrl }) => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  return (
+    <div style={{ height: '750px' }}>
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+        <Viewer
+          fileUrl={fileUrl}
+          plugins={[defaultLayoutPluginInstance]}
+          defaultScale={SpecialZoomLevel.PageFit}
+        />
+      </Worker>
+    </div>
+  );
+};
+
+export default PdfViewer;
