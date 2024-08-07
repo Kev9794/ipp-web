442f59dc811c9001980eedd473bced35dd7d45db
Kev9794 <k.natanael88@gmail.com>
2024-08-07 11:56:01 +0700

    add gtag

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { MainNavbar } from "./components/navbar";
 import MainFooter from "./components/footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16661703442"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16661703442');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <NextUIProvider>
           <main className="dark text-foreground bg-background">
