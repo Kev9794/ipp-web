4ae291e8440ef117d840a97a18da421a68b86620
Kev9794 <k.natanael88@gmail.com>
2024-08-09 15:51:11 +0700

    only gtag js setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,19 +16,40 @@
 //   return (
 //     <html lang="en">
 //       <Head>
-//         {/* <!-- Google Tag Manager --> */}
-//         <script>
-//           dangerouslySetInnerHTML=
-//           {{
+//         {/* <!-- Google Analytics (gtag.js) --> */}
+//         <script
+//           async
+//           src="https://www.googletagmanager.com/gtag/js?id=AW-16661703442"
+//         ></script>
+//         <script
+//           dangerouslySetInnerHTML={{
 //             __html: `
-//           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//         })(window,document,'script','dataLayer','GTM-MDVJF8Q6');
-// `,
+//               window.dataLayer = window.dataLayer || [];
+//               function gtag(){dataLayer.push(arguments);}
+//               gtag('js', new Date());
+//               gtag('config', 'AW-16661703442');
+//             `,
 //           }}
-//         </script>
+//         />
+//         {/* <!-- Event snippet for Purchase conversion page -->  */}
+//         <script> gtag('event', 'conversion', { 'send_to': 'AW-16661703442/WnSBCJy7l8gZEJLW9Yg-', 'transaction_id': '' }); </script>
+//         {/* <!-- Google Tag Manager --> */}
+//         <script
+//           dangerouslySetInnerHTML={{
+//             __html: `
+//               (function(w,d,s,l,i){
+//                 w[l]=w[l]||[];
+//                 w[l].push({'gtm.start':
+//                 new Date().getTime(),event:'gtm.js'});
+//                 var f=d.getElementsByTagName(s)[0],
+//                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+//                 j.async=true;
+//                 j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+//                 f.parentNode.insertBefore(j,f);
+//               })(window,document,'script','dataLayer','GTM-MDVJF8Q6');
+//             `,
+//           }}
+//         />
 //         {/* <!-- End Google Tag Manager --> */}
 //       </Head>
 //       <body className={inter.className}>
@@ -38,7 +59,7 @@
 //             src="https://www.googletagmanager.com/ns.html?id=GTM-MDVJF8Q6"
 //             height="0"
 //             width="0"
-//             style="display:none;visibility:hidden"
+//             style={{ display: "none", visibility: "hidden" }}
 //           ></iframe>
 //         </noscript>
 //         {/* <!-- End Google Tag Manager (noscript) --> */}
@@ -74,7 +95,7 @@ export default function RootLayout({ children }) {
         {/* <!-- Google Analytics (gtag.js) --> */}
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16661703442"
+          src="https://www.googletagmanager.com/gtag/js?id=YOUR_MEASUREMENT_ID"
         ></script>
         <script
           dangerouslySetInnerHTML={{
@@ -82,40 +103,13 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'AW-16661703442');
+              gtag('config', 'YOUR_MEASUREMENT_ID');
             `,
           }}
         />
-        {/* <!-- Google Tag Manager --> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MDVJF8Q6');
-            `,
-          }}
-        />
-        {/* <!-- End Google Tag Manager --> */}
+        {/* <!-- End Google Analytics --> */}
       </Head>
       <body className={inter.className}>
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MDVJF8Q6"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
         <NextUIProvider>
           <main className="dark text-foreground bg-background">
             <MainNavbar />
